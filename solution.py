a4b0476489cbac025c544698bee99bdd3fe60d36
L Ramos <LR@DN0a1f2e45.SUNet>
2018-03-09 18:44:28 -0800

    Created csv files and processed them

--- solution.py
+++ solution.py
@@ -1,1 +1,56 @@
-print "hello world"
+import csv
+
+#Time will do in minutes
+#Currently receive food by ID number
+
+#Get contents from files. Currently just uses the index of food
+#rather than name.
+def getContents(filename, col):
+	csvfile = open(filename, "rb")
+	datareader = csv.reader(csvfile)
+	contentList = []
+	for row in datareader:
+		if col == -1:
+			newRow = []
+			for elem in row:
+				if elem.isdigit():
+					newRow.append(int(elem))
+				else:
+					newRow.append(elem)
+			print newRow
+			contentList.append(newRow)
+		else:
+			if row[col].isdigit():
+				contentList.append(int(row[col]))
+			else:
+				contentList.append(row[col])
+
+	return contentList
+
+			
+
+def bloodSugarSim(inputList):
+	#sort inputList by time
+	print inputList
+	print sorted(inputList,key=lambda x: x[2])
+
+	#Start with Blood Sugar
+
+	for elem in inputList:
+		if elem[0] == 'f':
+			print "test"
+
+
+
+
+
+exerList = getContents("Exercise.csv", 2)
+foodList = getContents("FoodDB.csv", 2)
+testSet = getContents("test.csv", -1)
+
+bloodSugarSim(testSet)
+
+
+
+# print exerList
+# print foodList
---
+++ testGenerator.py
@@ -0,0 +1,24 @@
+import random
+
+hourLimit = 10
+foodTypes = 123
+exerTypes = 6
+
+
+
+for i in range(0, 10):
+	curStr = ""
+	numIndex = 0
+
+	if random.randint(0,3) < 3:
+		curStr += "f,"
+		numIndex = foodTypes
+	else:
+		curStr += "e,"
+		numIndex = exerTypes
+
+	curStr += str(random.randint(1,numIndex)) + ","
+	curStr += str(random.randint(0,60 * hourLimit))
+	print curStr
+
+	
